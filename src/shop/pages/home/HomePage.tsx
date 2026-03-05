5cf75f8a7a7cfc9efb4a15ff91e553d23fe7ebc3
Leonardo <mr_kunkel@hotmail.com>
2026-03-05 10:32:26 -0600

    Custom components generated header,footer,jumbotron & HomePage and ShopLayout changed

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
 
 export const HomePage = () => {
     return (
-        <div>
-            <h1>Hola Mundo</h1>
-            <h1 className="font-montserrat">Hola Mundo</h1>
-            <Button>Hola Mundo</Button>
-        </div>
+        <>
+            <CustomJumbotron title="Todos los productos" />
+        </>
     )
 }
