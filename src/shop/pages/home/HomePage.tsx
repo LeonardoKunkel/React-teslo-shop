f4b9e83327425eff859352702d567f729d46e410
Leonardo <mr_kunkel@hotmail.com>
2026-03-06 19:19:11 -0600

    Product Grid and other components created

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,12 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
 
 export const HomePage = () => {
     return (
         <>
             <CustomJumbotron title="Todos los productos" />
+
+            <CustomPagination totalPages={7} />
         </>
     )
 }
