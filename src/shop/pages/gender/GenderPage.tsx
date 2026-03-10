69a29cbe97274771b90987a547c8e335ae5b1d7a
Leonardo <mr_kunkel@hotmail.com>
2026-03-10 13:40:50 -0600

    CustomHeader & GenderPage updated

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,22 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+    const { gender } = useParams()
+
+    const genderLabel = gender === 'men' ? 'Hombres' : gender === 'women' ? 'Mujeres' : 'Niños';
+
     return (
-        <div>GenderPage</div>
+        <>
+            <CustomJumbotron title={`Productos para ${gender}`} />
+
+            <ProductsGrid products={products} />
+
+            <CustomPagination totalPages={7} />
+        </>
     )
 }
