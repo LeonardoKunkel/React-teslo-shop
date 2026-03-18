813662511186af7941b96fe8e59810d683f0930d
Leonardo <mr_kunkel@hotmail.com>
2026-03-17 21:33:39 -0600

    new files on auth folder

--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -4,13 +4,27 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { CustomLogo } from "@/components/custom/CustomLogo"
+import type { FormEvent } from "react"
 
 export const LoginPage = () => {
+
+
+    const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target as HTMLFormElement);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+    }
+
+
+
+
     return (
         <div className={"flex flex-col gap-6"}>
             <Card className="overflow-hidden p-0">
                 <CardContent className="grid p-0 md:grid-cols-2">
-                    <form className="p-6 md:p-8">
+                    <form className="p-6 md:p-8" onSubmit={handleLogin}>
                         <div className="flex flex-col gap-6">
                             <div className="flex flex-col items-center text-center">
 
@@ -26,6 +40,7 @@ export const LoginPage = () => {
                                 <Input
                                     id="email"
                                     type="email"
+                                    name="email"
                                     placeholder="m@example.com"
                                     required
                                 />
@@ -37,7 +52,7 @@ export const LoginPage = () => {
                                         ¿Olvidaste tu contraseña?
                                     </a>
                                 </div>
-                                <Input id="password" type="password" required placeholder="Contraseña" />
+                                <Input id="password" type="password" name="password" required placeholder="Contraseña" />
                             </div>
                             <Button type="submit" className="w-full">
                                 Ingresar
