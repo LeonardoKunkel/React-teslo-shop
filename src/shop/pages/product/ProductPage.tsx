182a86dba79ac4620dfb222fb23486dd8be8d24a
Leonardo <mr_kunkel@hotmail.com>
2026-03-19 20:09:21 -0600

    Zustand added

--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,6 +1,17 @@
+import { useCounterStore } from "@/auth/store/auth.store"
+import { Button } from "@/components/ui/button"
 
 export const ProductPage = () => {
+
+    const { inc, dec, count, incBy } = useCounterStore();
+
     return (
-        <div>ProductPage</div>
+        <div>
+            <h1 className="text-3xl font-montserrat">Count: {count}</h1>
+
+            <Button onClick={inc}>+1</Button>
+            <Button onClick={dec}>-1</Button>
+            <Button onClick={() => incBy(5)}>+51</Button>
+        </div>
     )
 }
