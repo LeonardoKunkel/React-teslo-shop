2db042a0d6a59d0bc6a30f90b5bdcc01405e3412
Leonardo <mr_kunkel@hotmail.com>
2026-04-02 13:26:27 -0600

    ProductForm & create-update.action updated

--- src/admin/actions/create-update-product.action.ts
+++ src/admin/actions/create-update-product.action.ts
@@ -8,17 +8,31 @@ export const createUpdateProductAction = async (
 
     await sleep(1500);
 
-    const { id, user, images = [], ...dataToPost } = productLike;
+    const { id, user, images = [], files = [], ...dataToPost } = productLike;
 
     const isCreating = id === 'new';
 
     dataToPost.stock = Number(dataToPost.stock || 0);
     dataToPost.price = Number(dataToPost.price || 0);
 
+    // Preparar imágenes
+    if (files.length > 0) {
+        const newImagesNames = await uploadFiles(files);
+        images.push(...newImagesNames);
+    }
+
+    const imagesToSave = images.map(image => {
+        if (image.includes('http')) return image.split('/').pop() || '';
+        return image;
+    })
+
     const { data } = await tesloApi<Product>({
         url: isCreating ? '/products' : `/products/${id}`,
         method: isCreating ? 'POST' : 'PATCH',
-        data: dataToPost
+        data: {
+            ...dataToPost,
+            images: imagesToSave
+        }
     });
 
     return {
@@ -28,5 +42,34 @@ export const createUpdateProductAction = async (
             return `${import.meta.env.VITE_API_URL}/files/product/${image}`
         })
     }
+
+}
+
+export interface FileUploadResponse {
+    secureUrl: string;
+    fileName: string;
+}
+
+
+const uploadFiles = async (files: File[]) => {
+
+    const uploadPromises = files.map(async file => {
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        // await tesloApi.post
+        const { data } = await tesloApi<FileUploadResponse>({
+            url: '/files/product',
+            method: 'POST',
+            data: formData
+        })
+
+        return data.fileName;
+    });
+
+
+    const uploadedFileNames = await Promise.all(uploadPromises);
+    return uploadedFileNames
 
 }
--- src/admin/pages/product/ui/ProductForm.tsx
+++ src/admin/pages/product/ui/ProductForm.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 import { useForm } from 'react-hook-form'
 import { AdminTitle } from "@/admin/components/AdminTitle";
@@ -40,7 +40,11 @@ export const ProductForm = ({ title, subTitle, product, onSubmit, isPending }: P
     });
 
     const labelInputRef = useRef<HTMLInputElement>(null);
-    const [files, setFiles] = useState<File[]>([])
+    const [files, setFiles] = useState<File[]>([]);
+
+    useEffect(() => {
+        setFiles([])
+    }, [product])
 
     const selectedSizes = watch('sizes');
     const selectedTags = watch('tags');
